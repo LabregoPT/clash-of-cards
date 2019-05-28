fdbbbffc33b0d16c77849884bf7b1528e4cafe2a
Jhon Edward Mora <jhonedmora@gmail.com>
2019-05-27 21:11:25 -0500

    Worked on classes Binary Search Tree and LinkedList

---
+++ src/data/BinarySearchTree.java
@@ -0,0 +1,261 @@
+package data;
+import java.util.Comparator;
+import java.util.Random;
+import model.*;
+
+/**
+ * Data structure that will contain the different Cards to the event in a Binary Search Tree.
+ * @author Jhon Edward Mora / Juan Andrés Orozco - Universidad ICESI
+ * @version 1.0a - 05/2019
+ */
+public class BinarySearchTree {
+	//Attributes
+	/**Sorting criteria for the binary search tree*/
+	private Comparator<Card> c;
+	
+	//Nested classes.
+	/**Nested class to handle each Card as a node in the binary search tree data structure*/
+	private class Node extends Card{
+		//Relations
+		/**Left child node of this node.*/
+		private Node left;
+		/**Right child node of this node.*/
+		private Node right;
+		/**Parent node of this node.*/
+		private Node p;
+		
+		//Methods
+		/**Simple constructor.*/
+		public Node(Card e) {
+			super(e.getName(), e.image, e.getAttributes());
+		}
+		
+		/**Returns the left child node of this node in the BST*/
+		public Node getLeft() {
+			return left;
+		}
+		
+		/**Returns the right child node of this node in the BST*/
+		public Node getRight() {
+			return right;
+		}
+		
+		/**Returns the parent node of this node in the BST.*/
+		public Node getP() {
+			return p;
+		}
+		
+		/**
+		 * Sets the left child node of this BNS to the one in the argument.
+		 * @param e node to be set as left child node.
+		 */
+		public void setLeft(Node e) {
+			left = e;
+		}
+		
+		/**
+		 * Sets the right child node of this BNS to the one in the argument.
+		 * @param e node to be set as right child node.
+		 */
+		public void setRight(Node e) {
+			right = e;
+		}
+		
+		/**
+		 * Sets the parent node of this BNS to the one in the argument.
+		 * @param e node to be set as parent node.
+		 */
+		public void setP(Node e) {
+			p = e;
+		}
+	}
+
+	//Relations
+	private Node root;
+	
+	//Methods
+	/**
+	 * Constructor method. Initialises an instance of the class.
+	 * @param comparator the sorting criteria for the binary search tree.
+	 */
+	public BinarySearchTree(Comparator<Card> comparator) {
+		this.c = comparator;
+	}
+	
+	/**
+	 * Checks if the Binary search tree is empty and then proceeds to add the given element to it.
+	 * @param element the element (node) to be added.
+	 */
+	public void add(Card element) {
+		Node toAdd = new Node(element);
+		if(root == null) {
+			root = toAdd;
+		}else {
+			add(toAdd, root);
+		}
+	}
+	
+	/**
+	 * Recursive function to add an element to a binary search tree.
+	 * @param a the element (node) to be added.
+	 * @param current the current element in which this recursive function is located.
+	 */
+	private void add(Node a, Node current) {
+		if(c.compare(a, current) <= 0) {
+			if(current.getLeft() == null) {
+				current.setLeft(a);
+				a.setP(current);
+			}else {
+				add(a, current.getLeft());
+			}
+		}else {
+			if(current.getRight() == null) {
+				current.setRight(a);
+				a.setP(current);
+			}else {
+				add(a, current.getRight());
+			}
+		}
+	}
+	
+	/**
+	 * Calls the recursive function to get the minimum element in this binary search tree.
+	 * @return the minimum element in this binary search tree.
+	 */
+	public Card getMin() {
+		return getMin(root);
+	}
+	
+	/**
+	 * Calls itself until finding a Node that has no child left Node (minimum element in the binary search tree)
+	 * @param current the current Node in the recursive function.
+	 * @return the element to the left of the argument's node, or itself if there's no element to the left.
+	 */
+	private Node getMin(Node current) {
+		if(current.getLeft() == null) {
+			return current;
+		}else {
+			return getMin(current.getLeft());
+		}
+	}
+	
+	/**
+	 * Calls the recursive function to get the maximum element in this binary search tree.
+	 * @return the maximum element in this binary search tree.
+	 */
+	public Card getMax() {
+		return getMax(root);
+	}
+	
+	/**
+	 * Calls itself until finding a Node that has no child right Node (maximum element in the binary search tree)
+	 * @param current the current Node in the recursive function.
+	 * @return the element to the right of the argument's node, or itself if there's no element to the right.
+	 */
+	private Node getMax(Node current) {
+		if(current.getRight() == null) {
+			return current;
+		}else {
+			return getMin(current.getRight());
+		}
+	}
+	
+	/**
+	 * Calls the recursive function to search a given element in the binary search tree.
+	 * @param toSearch the given element to be searched.
+	 * @return either the element matching the searched element, or null if the tree is empty or doesn't have the searched element.
+	 */
+	public Card search(Card toSearch) {
+		Node n = new Node(toSearch);
+		return search(n, root);
+	}
+	
+	/**
+	 * Calls itself until finding an element that is equal to the current checked node, or a null element.
+	 * @param toSearch the element to be searched.
+	 * @param current the current element to be compared with the searched element.
+	 * @return the left element of the current element if the searched element is lesser or equals than the current, right if not. May be null.
+	 */
+	private Node search(Node toSearch, Node current) {
+		if(current == null || c.compare(toSearch, current) == 0) {
+			return current;
+		}else {
+			if(c.compare(toSearch, current) <= 0) {
+				return search(toSearch, current.getLeft());
+			}else {
+				return search(toSearch, current.getRight());
+			}
+		}
+	}
+	
+	/**
+	 * Returns the root of the tree
+	 * @return the root of the tree with all of it's child nodes.
+	 */
+	public Card getRoot() {
+		return root;
+	}
+	
+	/**
+	 * Calls the recursive function to get a random node and returns it's result.
+	 * @return a random element of the tree.
+	 */
+	public Card getRandom() {
+		return getRandom(root);
+	}
+	
+	/**
+	 * Calls itself a random number of times until finding a node or a null node.
+	 * @param current the current node in the recursive function.
+	 * @return either the child left, child right or parent node of this node, or null.
+	 */
+	private Node getRandom(Node current) {
+		if(current == null) {
+			return current;
+		}else {
+			Random rnd = new Random();
+			int pointer = rnd.nextInt(3);
+			if(pointer == 0) {
+				return getRandom(current.getP());
+			}else if(pointer == 1) {
+				return getRandom(current.getLeft());
+			}else {
+				return getRandom(current.getRight());
+			}
+		}
+	}
+	
+	/**
+	 * Returns the successor in preorder of a given Card
+	 * @param current given Card
+	 * @return successor in preorder of the given Card
+	 */
+	public Card successorPreOrder(Card current) {
+		Node c = new Node(current);
+		return preOrderRec(c);
+	}
+	
+	/**
+	 * Returns the successor in preorder of a given Node
+	 * @param current given Node
+	 * @return successor in preorder of the given Node
+	 */
+	private Node preOrderRec(Node c) {
+		if(c.getLeft() != null) {
+			return c.getLeft();
+		}else {
+			Node p = c.getP();
+			while(p!= null && p.getRight() == c) {
+				c = c.getP();
+				p = p.getP();
+			}
+			if(p == null) {
+				return p;
+			}else {
+				return p.getRight();
+			}
+		}
+	}
+	
+	
+}
---
+++ src/data/LinkedList.java
@@ -0,0 +1,246 @@
+package data;
+
+import java.util.Comparator;
+import model.*;
+
+/**
+ * @author Jhon Edward Mora / Juan Andres Orozco - Universidad ICESI
+ * @version 0.1a - 05/2019
+ */
+public class LinkedList{
+
+	//Nested classes
+	/**This class handles each attendant as a node in a linked list. Does not modify the data on any of them.*/
+	private class Element extends Card{
+		
+		//Relations to handle each attendant as nodes in a linked list.
+		/**Next node of the linked list.*/
+		private Element next;
+		/**Previous node of the linked list.*/
+		private Element prev;
+		
+		//Methods
+		/**Simple constructor.
+		 * @param e the Attendant represented by this node.
+		 */
+		public Element(Card e) {
+			super(e.getName(), e.image, e.getAttributes());
+		}
+		
+		//Below methods are only to handle each attendant as a node in a linked list.
+		/**
+		 * Returns the next node to this node of the list.
+		 * @return the next node to this node of the list.
+		 */
+		public Element getNext() {
+			return next;
+		}
+		
+		/**
+		 * Returns the previous node to this node of the list.
+		 * @return the previous node to this node of the list.
+		 */
+		public Element getPrev() {
+			return prev;
+		}
+		
+		/**
+		 * Sets the next node of this node to the one in the argument.
+		 * @param e the node to be set as next node to this node.
+		 */
+		public void setNext(Element e) {
+			next = e;
+		}
+		
+		/**
+		 * Sets the previous node of this node to the one in the argument.
+		 * @param e the node to be set as previous node to this node. 
+		 */
+		public void setPrev(Element e) {
+			prev = e;
+		}
+		
+		/**
+		 * Creates an unlinked node identical to this element of the list.
+		 */
+		public Element clone() {
+			return new Element(this);
+		}
+	}
+
+	//Relations
+	/**First element of the list, also known as the list's head*/
+	private Element first;
+	
+	//Methods
+	/**
+	 * Adds an Attendant to the list in the list's tail (last element) and links it with its previous and next element.
+	 */
+	public void add(Card card) {
+		Element e = new Element(card);
+		if(first==null) {
+			first = e;
+		}else {
+			Element last = first;
+			while(last.getNext() != null) {
+				last = last.getNext();
+				}
+			e.setPrev(last);
+			last.setNext(e);
+			}
+	}
+
+	/**
+	 * True if the list's head is null, false if not.
+	 */
+	public boolean isEmpty() {
+		boolean empty = first==null;
+		return empty;
+	}
+	
+	/**
+	 * Returns the number of elements that this list currently has, defined as the number of times an Attendant can perform the getNext method until the next element is null.
+	 */
+	public int size() {
+		int totalElements = 0;
+		Element current = first;
+		if(current!=null) {
+			totalElements=1;
+			while(current.getNext() != null) {
+				current = current.getNext();
+				totalElements++;
+			}
+		}
+		return totalElements;
+	}
+	
+	/**
+	 * Returns the Attendant located at the given index.
+	 */
+	public Card get(int index) throws IndexOutOfBoundsException {
+		if(index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException("Tried to get index " + index + " when size is " + size());
+		}
+		Element found = first;
+		int currentIndex = 0;
+		while(currentIndex < index) {
+			currentIndex++;
+			found  = found.getNext();
+		}
+		return found.clone();
+	}
+
+	/**
+	 * Sets the Attendant located in the given index to the one in the parameter, and updates the next and prev fields of the next and previous elements of the replaced element.
+	 */
+	public void set(int index, Card element) throws IndexOutOfBoundsException {
+		Element el = new Element(element);
+		if(index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException("Tried to set " + element + " in index " + index+ "when size is " + size());
+		}
+		if(index == 0) {
+			if(first == null) {
+				first = el;
+			}else {
+				Element next = first.getNext();
+				next.setPrev(el);
+				el.setNext(next);
+				first = el;
+			}
+		}else {
+			Element found = first;
+			int currentIndex = 0;
+			while(currentIndex < index) {
+				currentIndex++;
+				found  = found.getNext();
+			}
+			if(found.getNext() != null) {
+				Element prev = found.getPrev();
+				Element next = found.getNext();
+				prev.setNext(el);
+				next.setPrev(el);
+				el.setNext(next);
+				el.setPrev(prev);
+			}else {
+				Element prev = found.getPrev();
+				el.setPrev(prev);
+				prev.setNext(el);
+			}
+		}
+	}
+
+	
+	/**
+	 * Starts recursion of the sorting methods. Most of the code taken from geeksforgeeks.com
+	 * @see https://www.geeksforgeeks.org/merge-sort/
+	 */
+	public void sort(Comparator<Card> c) {
+		sort(this, 0, this.size()-1,c);
+	}
+
+	/**Sorts the elements of the list.*/
+	private void sort(LinkedList list, int init, int last, Comparator<Card> c) {
+		if(init < last) {
+			int mid = (init+last)/2;
+			sort(list, init, mid,c);
+			sort(list, mid+1, last,c);
+			merge(list, init, mid, last,c);
+		}
+	}
+
+	/**Merges two sublists into one that, but now sorted.*/
+	private void merge(LinkedList list, int init, int mid, int last, Comparator<Card> c) {
+		//Sizes
+		int size1 = mid-init+1;
+		int size2 = last-mid;
+		//Sublists, halves
+		LinkedList sub1 = new LinkedList();
+		LinkedList sub2 = new LinkedList();
+		//Copy data
+		for(int i = 0; i<size1; i++) {
+			sub1.add(list.get(i));
+		}
+		for(int j = 0; j<size2; j++) {
+			sub2.add(list.get(j));
+		}
+		
+		//Merge halves
+		int i = 0, j = 0, k = init;
+		while(i<size1 && j < size2) {
+			if(c.compare(sub1.get(i),sub2.get(j)) <=0) {
+				list.set(k, sub1.get(i));
+				i++;
+			}else {
+				list.set(k, sub2.get(j));
+				j++;
+			}
+		}
+		//Add remaining elements
+		while(i<size1) {
+			list.set(k, sub1.get(i));
+			i++;k++;
+		}
+		while(j<size2) {
+			list.set(k, sub2.get(j));
+			j++;k++;
+		}
+	}
+
+	/**
+	 * Returns the first element of this list.
+	 */
+	public Card getFirst() {
+		return first;
+	}
+	
+	/**
+	 * Returns the list's tail, AKA last element
+	 */
+	public Card getLast() {
+		Element ret = first;
+		while(ret!=null && ret.getNext()!= null) {
+			ret = ret.getNext();
+		}
+		return ret;
+	}
+}
