ed96d505fb2f99240676c7fe537a0f3374889fd9
Jhon Edward Mora <jhonedmora@gmail.com>
2019-05-27 21:00:26 -0500

    Finished class Card

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,10 +1,9 @@
 package model;
 /**
- * This class represent the single card
+ * This class represent each of the cards of the game.
  * @author Andres Orozco && Jhon Edward Mora
  *
  */
-
 public class Card{
 	
 	/**
@@ -31,22 +30,40 @@ public class Card{
 		image = im;
 	}
 	
+	/**
+	 * Returns the attributes of this card.
+	 * @return the attributes of this card.
+	 */
 	public Attribute[] getAttributes() {
 		return attributes;
 	}
 	
+	/**
+	 * Makes two cards fight by comparing their attributes. Returns a positive number if this card wins the fight, a negative if it loses and 0 if its a draw.
+	 * <br>If both this card and the enemy card have the searched attribute, returns the result of the comparison of them.
+	 * <br>If this card has the attribute and the other doesn't, immediately wins the match.
+	 * <br>If this card doesn't have the attribute and the enemy does, immediately loses the match.
+	 * <br>If none of the cards have the attribute, its a draw.
+	 * @param enemy the enemy card to have a match with.
+	 * @param attribute the attribute that will rule the match.
+	 * @return a positive number if this card wins, negative if it loses, and 0 if its a draw.
+	 */
 	public int fight(Card enemy, String attribute) {
 		int r = 0;
 		if(hasAttribute(attribute) && enemy.hasAttribute(attribute)) {
 			r = getAttribute(attribute).compareTo(enemy.getAttribute(attribute));
 		}else if(hasAttribute(attribute) && !enemy.hasAttribute(attribute)) {
 			r = 1;
-		}else {
+		}else if(!hasAttribute(attribute) && enemy.hasAttribute(attribute)){
 			r = -1;
 		}
 		return r;
 	}
 	
+	/**
+	 * Returns the name of this card.
+	 * @return the name of this card.
+	 */
 	public String getName() {
 		return name;
 	}
