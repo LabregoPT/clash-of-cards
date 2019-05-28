8de139892ba5950e962d41c7aff1c153eadb0b6f
Jhon Edward Mora <jhonedmora@gmail.com>
2019-05-27 20:51:36 -0500

    Worked on class Card

--- src/model/Card.java
+++ src/model/Card.java
@@ -5,7 +5,7 @@ package model;
  *
  */
 
-public class Card implements Comparable<Card>{
+public class Card{
 	
 	/**
 	 * Represents the card name
@@ -20,18 +20,92 @@ public class Card implements Comparable<Card>{
 	/**The attributes of this card. From 1 to 3 per card.*/
 	private Attribute[] attributes;
 	
+	/**
+	 * Constructor method.
+	 * @param n the name of the card
+	 * @param im the location of the image of this card.
+	 * @param a the attributes of this card.
+	 */
 	public Card(String n, String im, Attribute[] a) {
 		name = n;
 		image = im;
 	}
 	
+	public Attribute[] getAttributes() {
+		return attributes;
+	}
+	
+	public int fight(Card enemy, String attribute) {
+		int r = 0;
+		if(hasAttribute(attribute) && enemy.hasAttribute(attribute)) {
+			r = getAttribute(attribute).compareTo(enemy.getAttribute(attribute));
+		}else if(hasAttribute(attribute) && !enemy.hasAttribute(attribute)) {
+			r = 1;
+		}else {
+			r = -1;
+		}
+		return r;
+	}
+	
+	public String getName() {
+		return name;
+	}
 	
 	/**
-	 * This method compares the card in a lexicographic order
+	 * Determines whether a card has an attribute or not. 
+	 * @param attribute attribute to be searched.
+	 * @return true if any of the three attributes has a name that matches with the searched attribute, false if not.
 	 */
-	@Override
-	public int compareTo(Card e) {
-		return 0;
+	public boolean hasAttribute(String attribute) {
+		if(attributes[0].getName().equals(attribute)) {
+			return true;
+		}else if(attributes[1] != null) {
+			if(attributes[1].getName().equals(attribute)) {
+				return true;
+			}else {
+				return false;
+			}
+		}else if(attributes[2] != null) {
+			if(attributes[2].getName().equals(attribute)) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+		
 	}
-
+	
+	/**
+	 * Gets the attribute matching the given name. May return null.
+	 * @param attribute the searched attribute name.
+	 * @return an attribute with a name matching the searched attribute name.
+	 */
+	public Attribute getAttribute(String attribute) {
+		Attribute toR = null;
+		if(attributes[0].getName().equals(attribute)) {
+			toR = attributes[0];
+		}else {
+			if(attributes[1] != null) {
+				if(attributes[1].getName().equals(attribute)) {
+					toR = attributes[1];
+				}else {
+					if(attributes[2] != null) {
+						if(attributes[2].getName().equals(attribute)) {
+							toR = attributes[2];
+						}
+					}
+				}
+			}else {
+				if(attributes[2] != null) {
+					if(attributes[2].getName().equals(attribute)) {
+						toR = attributes[2];
+					}
+				}
+			}
+		}
+		return toR;
+	}
+	
 }
