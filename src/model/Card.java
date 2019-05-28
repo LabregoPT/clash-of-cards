857300242c67c70b84765ac23d235620bff451f6
Jhon Edward Mora <jhonedmora@gmail.com>
2019-05-27 19:57:31 -0500

    Corrected and improved class diagram

--- src/model/Card.java
+++ src/model/Card.java
@@ -11,29 +11,17 @@ public class Card implements Comparable<Card>{
 	 * Represents the card name
 	 */
 	private String name;
-	/**
-	 * Represent the attack points as an integer number
-	 */
-	private int attack;
-	/**
-	 * Represent the defense points as an integer number
-	 */
-	private int defense;
-	/**
-	 * Contructor Method
-	 * @param n Represent the card name
-	 * @param a Represent the attack points
-	 * @param d Represent the defense points
-	 */
-	public Card(String n,int a,int d) {
+	
+	
+	
+	public Card(String n) {
 		name = n;
-		attack=a;
-		defense=d;
 	}
+	
+	
 	/**
-	 * This method compare the card in a lexicographic order
+	 * This method compares the card in a lexicographic order
 	 */
-
 	@Override
 	public int compareTo(Card e) {
 		return 0;
